1a8aa31fb866e76d5c91dd0d9d7fd1d2f5e6941e
ArhyParh <zescoarty@gmail.com>
2023-04-27 00:49:30 +0300

    Решил 2ю задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -13,18 +13,11 @@ void Main()
 }
 void FindThirdNumber(int n)
 {
-    if (n < 100)
-    {
-        Console.WriteLine("Третьей цифры нет.");
-    }
-    else
-    {
-        while (n > 1000)
-        {
-            n = n - 1000;
-            Console.WriteLine(n);
-        }
-        int m = n % 10;
-        Console.WriteLine(m);
-    }
+    if (n < 100) { Console.WriteLine("Третьей цифры нет."); }
+    else if (1000 > n && n > 100) { Console.WriteLine(n % 10); }
+    else if (10000 > n && n > 1000) { Console.WriteLine((n % 100)/10); }
+    else if (100000 > n && n > 10000) { Console.WriteLine((n % 1000)/100); }
+    else if (1000000 > n && n > 100000) { Console.WriteLine((n % 10000)/1000); }
+    else if (10000000 > n && n > 1000000) { Console.WriteLine((n % 100000)/10000); }
+    else if (100000000 > n && n > 10000000) { Console.WriteLine((n % 1000000)/100000); }
 }
