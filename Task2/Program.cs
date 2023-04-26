a19e5b07ccb0531e5cb9201d7878d40e8f5bf79b
ArhyParh <zescoarty@gmail.com>
2023-04-27 01:13:42 +0300

    Решил вторую задачу еще раз

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,16 +8,20 @@ void Main()
 {
     Console.WriteLine("Введите любое число: ");
     int a = int.Parse(Console.ReadLine());
-    FindThirdNumber(a);
-
+    if (IsNumberValid(a) == true) { Console.WriteLine("Третьей цифры нет!"); }
+    else { Console.WriteLine(FindThirdNumber(a)); }
 }
-void FindThirdNumber(int n)
+int FindThirdNumber(int n)
 {
-    if (n < 100) { Console.WriteLine("Третьей цифры нет."); }
-    else if (1000 > n && n > 100) { Console.WriteLine(n % 10); }
-    else if (10000 > n && n > 1000) { Console.WriteLine((n % 100)/10); }
-    else if (100000 > n && n > 10000) { Console.WriteLine((n % 1000)/100); }
-    else if (1000000 > n && n > 100000) { Console.WriteLine((n % 10000)/1000); }
-    else if (10000000 > n && n > 1000000) { Console.WriteLine((n % 100000)/10000); }
-    else if (100000000 > n && n > 10000000) { Console.WriteLine((n % 1000000)/100000); }
+    while (n > 999)
+    {
+        n /= 10;
+    }
+    return n % 10;
+}
+
+bool IsNumberValid(int n)
+{
+    if (n < 100) return true;
+    else return false;
 }
