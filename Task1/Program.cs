0a1657692d91458a1ffe3cd4b880914826878771
ArhyParh <zescoarty@gmail.com>
2023-04-27 00:16:45 +0300

    Добавил решение первой задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Main();
+void Main()
+{
+int a = new Random().Next(100,1000);
+Console.WriteLine(a);
+Console.WriteLine(FindSecondNumberOf(a));
+}
+int FindSecondNumberOf(int n)
+{
+    while(n>100){
+        n = n-100;
+    }
+    int rand = n/10;
+    return rand;
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Main();
+void Main()
+{
+    Console.WriteLine("Введите любое число: ");
+    int a = int.Parse(Console.ReadLine());
+    FindThirdNumber(a);
+
+}
+void FindThirdNumber(int n)
+{
+    if (n < 100)
+    {
+        Console.WriteLine("Третьей цифры нет.");
+    }
+    else
+    {
+        while (n > 1000)
+        {
+            n = n - 1000;
+            Console.WriteLine(n);
+        }
+        int m = n % 10;
+        Console.WriteLine(m);
+    }
+}
